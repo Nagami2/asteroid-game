a877cd19f2bf698fe21a8d39a9fcec56c2e1beed
Nagami2 <nagababukatakam2@gmail.com>
2025-10-01 14:03:03 -0400

    created basic game loop

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
+from constants import *
 
 def main():
-    print("Hello from asteroid-game!")
+    # initialize pygame
+    pygame.init()
+
+    # create the screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        # event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return # exit the main function, which ends the game
+        # drawing
+        screen.fill("black") # fill the screen with a black color
+
+        # update the display
+        pygame.display.flip() # refresh the screen to show the new drawings
 
 
 if __name__ == "__main__":
