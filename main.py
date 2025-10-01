7a42627f4a15332f3e8b4493559a85ddde17fac0
Nagami2 <nagababukatakam2@gmail.com>
2025-10-01 16:36:32 -0400

    add shot destroy asteroids

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from constants import *
 
 from player import Player
-from asteroidfield import AsteroidField
 from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -15,11 +17,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
 
     #setting the containers for the player class, before creating any player instance
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
 
     # create a player object in the middle of the screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -33,9 +38,28 @@ def main():
         # player.update(dt) # calling player's update method each frame
         updatable.update(dt) # update all updatable objects
 
+        # check for collisions between player and asteroids
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit() 
+
+        # check for collisions between shots and asteroids
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()  # remove the asteroid
+                    shot.kill()      # remove the shot
+
+        # remove shots that are off-screen to prevent memory leaks
+        for shot in shots:
+            if (shot.position.x < 0 or shot.position.x > SCREEN_WIDTH or 
+                shot.position.y < 0 or shot.position.y > SCREEN_HEIGHT):
+                shot.kill()
+
         # drawing comes after updating
         screen.fill("black") # fill the screen with a black color
-        # player.draw(screen)
+
         # loop through all drawables
         for thing in drawable:
             thing.draw(screen)
--- player.py
+++ player.py
@@ -1,12 +1,14 @@
-from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
-
 import pygame
+
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x , y, PLAYER_RADIUS)
         self.rotation = 0 # adding a rotation field
+        self.shoot_timer = 0
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -14,9 +16,20 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        # create a new shot at the player's position
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        shot  = Shot(self.position.x, self.position.y)
+        # set the shot's velocity to move forward from the ship
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED 
     
     # update method to check for key presses
     def update(self, dt):
+        # decrease the timer by dt every frame
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         # call rotate() with a negative dt to turn left
@@ -31,6 +44,11 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+        # spacebar input to call shoot method
+        if keys[pygame.K_SPACE] and self.shoot_timer <= 0:
+            self.shoot()
+
+    
     # method to calculate the triangle's points
     def triangle(self):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
